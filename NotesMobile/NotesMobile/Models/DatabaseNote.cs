96373bfb9c3e09738928f50e89b3ed5869ad6caa
Mikhail-Kavzov <kavzovm@mail.ru>
2023-02-07 09:49:59 +0300

    Добавьте файлы проекта.

---
+++ NotesMobile/NotesMobile/Models/DatabaseNote.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesMobile.Models
+{
+    public class DatabaseNote : Note
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ NotesMobile/NotesMobile/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesMobile.Models
+{
+    public class Note
+    {
+        public string Header { get; set; } 
+        public string Text { get; set; }
+    }
+}
