32b7929925f36774bb1561f996f2c7444a4f9056
Mikhail-Kavzov <kavzovm@mail.ru>
2023-02-07 10:53:22 +0300

    fix sqlite repo

--- NotesMobile/NotesMobile/Repository/Implementation/SQLiteRepository.cs
+++ NotesMobile/NotesMobile/Repository/Implementation/SQLiteRepository.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using NotesMobile.Models;
+using NotesMobile.Repository.Interfaces;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NotesMobile.Repository.Implementation
 {
-    internal class SQLiteRepository
+    public class SQLiteRepository : ISQLRepository<DatabaseNote>
     {
+        private readonly SQLiteAsyncConnection _db;
+
+        public SQLiteRepository(string connectionString)
+        {
+            _db = new SQLiteAsyncConnection(connectionString);
+        }
+
+        public async Task CreateTableAsync()
+        {
+            await _db.CreateTableAsync<DatabaseNote>();
+        }
+
+        public async Task DeleteAsync(DatabaseNote item)
+        {
+            await _db.DeleteAsync(item);
+        }
+
+        public async Task<IEnumerable<DatabaseNote>> GetAllNotesAsync(int skip, int take)
+        {
+            return await _db.Table<DatabaseNote>().
+                 Skip(skip * take).Take(take).
+                 ToListAsync();
+        }
+
+        public async Task<IEnumerable<DatabaseNote>> GetNotesAsync(string searchString,
+            int skip, int take)
+        {
+            return await _db.Table<DatabaseNote>().
+                 Where(n => n.Header.Contains(searchString) || n.Text.Contains(searchString)).
+                 Skip(skip * take).Take(take).
+                 ToListAsync();
+        }
+
+        public async Task<int> SaveAsync(DatabaseNote item)
+        {
+            if (item.Id == 0)
+            {
+                return await _db.InsertAsync(item);
+            }
+            await _db.UpdateAsync(item);
+            return item.Id;
+        }
     }
 }
